ff04593d05473c3ce1fea3d6cb786537ec7d64f2
filip-urban <filipurban93@gmail.com>
2025-06-01 13:23:06 +0200

    add main.sh and main.py

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+
+def main():
+    test_text_node = TextNode("This is a text node", "link", "www.textlink.com")
+    print(test_text_node)
+
+
+if __name__ == "__main__":
+    main()
