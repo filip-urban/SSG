013435cb27fb3c5919328032f8ca797088704fd7
filip-urban <filipurban93@gmail.com>
2025-06-20 09:41:10 +0200

    add tests for extract_markdown_links

--- src/tests/test_extract_markdown.py
+++ src/tests/test_extract_markdown.py
@@ -13,6 +13,23 @@ class Testextract_markdown(unittest.TestCase):
         )
         self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
 
+    def test_extract_markdown_image_and_link(self):
+        text = "This is a text with a link [link](www.google.com) and with an image ![image](https://i.imgur.com/zjjcJKZ.png)"
+        matches_link = extract_markdown_links(text)
+        matches_image = extract_markdown_images(text)
+        self.assertListEqual([("link", "www.google.com")], matches_link)
+        self.assertListEqual(
+            [("image", "https://i.imgur.com/zjjcJKZ.png")], matches_image
+        )
+
+    def test_extract_markdown_link(self):
+        matches = extract_markdown_links(
+            "This is a text with a link [link](www.google.com) and with an another link [link](www.youtube.com))"
+        )
+        self.assertListEqual(
+            [("link", "www.google.com"), ("link", "www.youtube.com")], matches
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
