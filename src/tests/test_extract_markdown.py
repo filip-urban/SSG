7fd485ca1f7a067ace3c5f01b6766b7282baaf34
filip-urban <filipurban93@gmail.com>
2025-06-20 09:43:57 +0200

    small changes to tests for extract_markdown_links

--- src/tests/test_extract_markdown.py
+++ src/tests/test_extract_markdown.py
@@ -9,9 +9,11 @@ from extract_markdown import extract_markdown_images, extract_markdown_links
 class Testextract_markdown(unittest.TestCase):
     def test_extract_markdown_images(self):
         matches = extract_markdown_images(
-            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+            "This is text with an ![some image](https://i.imgur.com/zjjcJKZ.png)"
         )
-        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+        self.assertListEqual(
+            [("some image", "https://i.imgur.com/zjjcJKZ.png")], matches
+        )
 
     def test_extract_markdown_image_and_link(self):
         text = "This is a text with a link [link](www.google.com) and with an image ![image](https://i.imgur.com/zjjcJKZ.png)"
@@ -24,10 +26,10 @@ class Testextract_markdown(unittest.TestCase):
 
     def test_extract_markdown_link(self):
         matches = extract_markdown_links(
-            "This is a text with a link [link](www.google.com) and with an another link [link](www.youtube.com))"
+            "This is a text with a link [link](www.google.com) and with an another link [different link](www.youtube.com))"
         )
         self.assertListEqual(
-            [("link", "www.google.com"), ("link", "www.youtube.com")], matches
+            [("link", "www.google.com"), ("different link", "www.youtube.com")], matches
         )
 
 
