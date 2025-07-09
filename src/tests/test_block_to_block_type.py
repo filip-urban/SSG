dc678ec083282b3fab79f7293f8cae58a85e8058
filip-urban <filipurban93@gmail.com>
2025-07-09 13:51:01 +0200

    add block_to_block_type

---
+++ src/tests/test_block_to_block_type.py
@@ -0,0 +1,28 @@
+import unittest
+import sys
+
+sys.path.append("src")
+
+from blocktype import block_to_block_type, BlockType
+
+
+class Testblock_to_block_type(unittest.TestCase):
+    def test_block_to_block_type(self):
+        heading = "#heading"
+        code = "```some code```"
+        quote = ">some quote"
+        u_list = """- first item
+- second item
+- third item"""
+        o_list = """1. first item
+2. second item
+3. third item"""
+        self.assertEqual(block_to_block_type(heading), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(code), BlockType.CODE)
+        self.assertEqual(block_to_block_type(quote), BlockType.QUOTE)
+        self.assertEqual(block_to_block_type(u_list), BlockType.UNORDERED_LIST)
+        self.assertEqual(block_to_block_type(o_list), BlockType.ORDERED_LIST)
+
+
+if __name__ == "__main__":
+    unittest.main
