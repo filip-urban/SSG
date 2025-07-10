0b7ca2970734a6b7833174ad1b474958945a51d1
filip-urban <filipurban93@gmail.com>
2025-07-10 22:15:08 +0200

    add more tests

--- src/tests/test_split_nodes_delimiter.py
+++ src/tests/test_split_nodes_delimiter.py
@@ -28,6 +28,27 @@ class Testsplit_nodes_delimiter(unittest.TestCase):
             ],
         )
 
+    def test_nothing_to_split(self):
+        node = TextNode("**Bolded text** and normal text", TextType.TEXT)
+        self.assertEqual(
+            split_nodes_delimiter([node], "_", TextType.ITALIC),
+            [node],
+        )
+
+    def test_inline_multi_line(self):
+        text = """This is **bolded** paragraph
+with text
+and more text"""
+        node = TextNode(text, TextType.TEXT)
+        self.assertEqual(
+            split_nodes_delimiter([node], "**", TextType.BOLD),
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("bolded", TextType.BOLD),
+                TextNode(" paragraph\nwith text\nand more text", TextType.TEXT),
+            ],
+        )
+
     def test_inline_middle(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         self.assertEqual(
