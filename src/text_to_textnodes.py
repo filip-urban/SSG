e498695549cd653c6256aed002d121f40510bac0
filip-urban <filipurban93@gmail.com>
2025-07-10 10:17:19 +0200

    don't create additional variables

--- src/text_to_textnodes.py
+++ src/text_to_textnodes.py
@@ -3,16 +3,8 @@ from textnode import TextNode, TextType
 
 
 def text_to_textnodes(text):
-    links_and_images = split_nodes_link(
-        split_nodes_image([TextNode(text, TextType.TEXT)])
-    )
-    links_and_images_and_bold = split_nodes_delimiter(
-        links_and_images, "**", TextType.BOLD
-    )
-    links_and_images_and_emphasis = split_nodes_delimiter(
-        links_and_images_and_bold, "_", TextType.ITALIC
-    )
-    links_and_images_emphasis_and_code = split_nodes_delimiter(
-        links_and_images_and_emphasis, "`", TextType.CODE
-    )
-    return links_and_images_emphasis_and_code
+    split_nodes = split_nodes_link(split_nodes_image([TextNode(text, TextType.TEXT)]))
+    split_nodes = split_nodes_delimiter(split_nodes, "**", TextType.BOLD)
+    split_nodes = split_nodes_delimiter(split_nodes, "_", TextType.ITALIC)
+    split_nodes = split_nodes_delimiter(split_nodes, "`", TextType.CODE)
+    return split_nodes
