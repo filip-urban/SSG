7eefd8c1365788c0902aeecd30b4a1a07da6406b
filip-urban <filipurban93@gmail.com>
2025-07-10 09:45:20 +0200

    fix constructor

--- src/parentnode.py
+++ src/parentnode.py
@@ -3,9 +3,7 @@ from htmlnode import HTMLNode
 
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
-        self.tag = tag
-        self.children = children
-        self.props = props
+        super().__init__(tag, None, children, props)
 
     def to_html(self):
         if not self.tag:
