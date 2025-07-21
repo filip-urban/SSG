d9109f0a0b523a0ca79ba0f8db9d201835fadcf4
filip-urban <filipurban93@gmail.com>
2025-07-21 17:51:42 +0200

    add generate_page and some html

---
+++ src/generate_html.py
@@ -0,0 +1,30 @@
+import sys
+import pathlib
+
+from block_to_html import markdown_to_html_node
+from extract_markdown import extract_title
+
+
+def read_file(filename):
+    try:
+        with open(filename, "r") as file_h:
+            return file_h.read()
+    except OSError as err:
+        print(err)
+        sys.exit(1)
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    md_file = read_file(from_path)
+    template = read_file(template_path)
+    html = markdown_to_html_node(md_file).to_html()
+    title = extract_title(md_file)
+    html_page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    pathlib.path(dest_path).mkdir(prents=True, exist_ok=True)
+    try:
+        with open(dest_path, "w") as dest_file_h:
+            dest_file_h.write(html_page)
+    except OSError as err:
+        print(err)
+        sys.exit(1)
