a0fd2f63f4deb71056bfd3f558c65ca1c47b7feb
filip-urban <filipurban93@gmail.com>
2025-07-21 18:26:40 +0200

    error fixes

--- src/generate_html.py
+++ src/generate_html.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pathlib
 
 from block_to_html import markdown_to_html_node
@@ -19,11 +20,11 @@ def generate_page(from_path, template_path, dest_path):
     md_file = read_file(from_path)
     template = read_file(template_path)
     html = markdown_to_html_node(md_file).to_html()
-    title = extract_title(md_file)
+    title = extract_title(from_path)
     html_page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
-    pathlib.path(dest_path).mkdir(prents=True, exist_ok=True)
+    pathlib.Path(os.path.dirname(dest_path)).mkdir(parents=True, exist_ok=True)
     try:
-        with open(dest_path, "w") as dest_file_h:
+        with open(dest_path, "w+") as dest_file_h:
             dest_file_h.write(html_page)
     except OSError as err:
         print(err)
