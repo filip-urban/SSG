fc94cc466a3b43e373c2e0dbd1c3995b076a7603
filip-urban <filipurban93@gmail.com>
2025-07-21 17:52:10 +0200

    generate html in main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
-from copy_content import *
-import os
+from copy_dir_contents import clean_directory, copy_files
+from generate_html import generate_page
 
 
 def main():
+    clean_directory("public")
     copy_files("static/", "public/")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 
 if __name__ == "__main__":
